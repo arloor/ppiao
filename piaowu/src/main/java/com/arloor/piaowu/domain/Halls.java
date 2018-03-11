e6285bbb58d6e448e4de5126d297cdb62de3c91a
arloor <admin@arloor.com>
2018-03-11 23:14:30 +0800

    发布计划功能完成

---
+++ piaowu/src/main/java/com/arloor/piaowu/domain/Halls.java
@@ -0,0 +1,65 @@
+package com.arloor.piaowu.domain;
+
+
+public class Halls {
+
+  private String vname;
+  private String hname;
+  private String stype;
+  private long rownum;
+  private long colnum;
+  private String status;
+
+
+  public String getVname() {
+    return vname;
+  }
+
+  public void setVname(String vname) {
+    this.vname = vname;
+  }
+
+
+  public String getHname() {
+    return hname;
+  }
+
+  public void setHname(String hname) {
+    this.hname = hname;
+  }
+
+
+  public String getStype() {
+    return stype;
+  }
+
+  public void setStype(String stype) {
+    this.stype = stype;
+  }
+
+
+  public long getRownum() {
+    return rownum;
+  }
+
+  public void setRownum(long rownum) {
+    this.rownum = rownum;
+  }
+
+
+  public long getColnum() {
+    return colnum;
+  }
+
+  public void setColnum(long colnum) {
+    this.colnum = colnum;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
