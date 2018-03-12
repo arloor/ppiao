cd586267281a838e74004c028b881095789eb779
arloor <admin@arloor.com>
2018-03-12 10:13:26 +0800

    增加一个domain

---
+++ piaowu/src/main/java/com/arloor/piaowu/domain/Play.java
@@ -0,0 +1,77 @@
+package com.arloor.piaowu.domain;
+
+
+public class Play {
+
+  private String pname;
+  private String vname;
+  private String hname;
+  private String pdate;
+  private String ptime;
+  private String ptype;
+  private String pinfo;
+
+
+  public String getPname() {
+    return pname;
+  }
+
+  public void setPname(String pname) {
+    this.pname = pname;
+  }
+
+
+  public String getVname() {
+    return vname;
+  }
+
+  public void setVname(String vname) {
+    this.vname = vname;
+  }
+
+
+  public String getHname() {
+    return hname;
+  }
+
+  public void setHname(String hname) {
+    this.hname = hname;
+  }
+
+
+  public String getPdate() {
+    return pdate;
+  }
+
+  public void setPdate(String pdate) {
+    this.pdate = pdate;
+  }
+
+
+  public String getPtime() {
+    return ptime;
+  }
+
+  public void setPtime(String ptime) {
+    this.ptime = ptime;
+  }
+
+
+  public String getPtype() {
+    return ptype;
+  }
+
+  public void setPtype(String ptype) {
+    this.ptype = ptype;
+  }
+
+
+  public String getPinfo() {
+    return pinfo;
+  }
+
+  public void setPinfo(String pinfo) {
+    this.pinfo = pinfo;
+  }
+
+}
