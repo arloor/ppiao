eb5f0e12f52705ec7dee2318b6b3331e238c3843
arloor <admin@arloor.com>
2018-03-21 19:02:17 +0800

    暂存

--- piaowu/src/main/java/com/arloor/piaowu/dao/AdminDao.java
+++ piaowu/src/main/java/com/arloor/piaowu/dao/AdminDao.java
@@ -1,10 +1,15 @@
 package com.arloor.piaowu.dao;
 
 import com.arloor.piaowu.domain.Admin;
+import com.arloor.piaowu.domain.Halls;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface AdminDao {
     Admin searchAdmin(@Param("aname") String s);
+
+    List<Halls> selectHallsbyVname(@Param("vname")String vname);
 }
--- piaowu/src/main/java/com/arloor/piaowu/model/TocheckVenues.java
+++ piaowu/src/main/java/com/arloor/piaowu/model/TocheckVenues.java
@@ -1,4 +1,40 @@
 package com.arloor.piaowu.model;
 
 public class TocheckVenues {
+    private String vname;
+    private String vaddr;
+    private String vinfo;
+    private String seats;
+
+    public String getVname() {
+        return vname;
+    }
+
+    public void setVname(String vname) {
+        this.vname = vname;
+    }
+
+    public String getVaddr() {
+        return vaddr;
+    }
+
+    public void setVaddr(String vaddr) {
+        this.vaddr = vaddr;
+    }
+
+    public String getVinfo() {
+        return vinfo;
+    }
+
+    public void setVinfo(String vinfo) {
+        this.vinfo = vinfo;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public void setSeats(String seats) {
+        this.seats = seats;
+    }
 }
